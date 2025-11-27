c46052427c3e6219bf30ddbe261ab445184ac9fe
RJ Martin <rjmartin1995@gmail.com>
2025-11-27 11:10:10 -0500

    cleaning up Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,53 +1,22 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import frc.robot.subsystems.Effector;
 import frc.robot.subsystems.Lights;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
-import edu.wpi.first.cameraserver.CameraServer;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 import com.pathplanner.lib.commands.PathfindingCommand;
-import com.pathplanner.lib.path.PathConstraints;
 
 import au.grapplerobotics.CanBridge;
-
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
-import edu.wpi.first.cameraserver.CameraServer;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 import com.ctre.phoenix6.controls.NeutralOut;
-import com.pathplanner.lib.commands.PathfindingCommand;
 
-import au.grapplerobotics.CanBridge;
-import au.grapplerobotics.LaserCan;
 import frc.robot.subsystems.Hang;
 
 
@@ -57,18 +26,9 @@ public class Robot extends TimedRobot {
     private Vision vision;
     private CommandSwerveDrivetrain drivetrain;
     private final Lights lights = new Lights();
-    private XboxController m_joystick = new XboxController(1);
-
-    private final LaserCan elevatorTop = new LaserCan(1);
 
     private final NeutralOut m_brake = new NeutralOut();
 
-
-
-    //private Spark intakeActuator = new Spark(7);
-
-    Timer intakeTimer = new Timer();
-    static Timer liftTimer = new Timer();
     Timer gc = new Timer();
 
 
@@ -78,13 +38,9 @@ public class Robot extends TimedRobot {
         var inst = NetworkTableInstance.getDefault();
         inst.startServer();
         m_robotContainer = new RobotContainer();
-        // CameraServer.startAutomaticCapture();
         PathfindingCommand.warmupCommand().schedule();
-        // elevator = new Elevator();
 
         CanBridge.runTCP();
-
-        // lights.runPattern(lights.purpleGoldStep).schedule();
     }
 
     
@@ -102,9 +58,6 @@ public class Robot extends TimedRobot {
         m_robotContainer = new RobotContainer();
         drivetrain = m_robotContainer.drivetrain;
         vision = m_robotContainer.vision;
-    
-        CanBridge.runTCP();
-        CameraServer.startAutomaticCapture();
     }
 
     
